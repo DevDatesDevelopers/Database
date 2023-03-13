b1e8bd0ba87160af78940797375c8ba783ef5289
Alexander Boev <naidobrixxjr@gmail.com>
2023-03-13 13:11:45 +0200

    Initial commit

---
+++ SQL Roles and Access.sql
@@ -0,0 +1,11 @@
+CREATE LOGIN milenkov WITH PASSWORD = '********';
+
+CREATE USER BoyanMilenkov FOR LOGIN milenkov;
+
+exec sys.sp_addrolemember 'db_datareader', 'BoyanMilenkov';
+
+CREATE LOGIN class WITH PASSWORD = 'DevDatesPublic123';
+
+CREATE USER PublicAccess FOR LOGIN class
+
+exec sys.sp_addrolemember 'public', 'PublicAccess';
